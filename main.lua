3c42f022d4574417d8d85d6f63cd89228d3dfca7
meohyphenw <zyn890512@outlook.com>
2023-05-20 23:34:33 +0800

    忘记删图片了

--- main.lua
+++ main.lua
@@ -1,15 +1,7 @@
 flandre = require 'flandre'
 
 function love.load()
-    _load_image('www.png', 'www')
-    _new(flandre.sprite)
-    _last.image = _p.image.www
-    _task.new(_last, function ()
-        for i = 1, 100 do
-            _last.x = _last.x + 1
-            coroutine.yield()
-        end
-    end)
+
 end
 
 function love.update(dt)
