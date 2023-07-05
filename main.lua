22bbe31fd09c448f058ab4309d93c37acbb5a4fb
meohyphenw <zyn890512@outlook.com>
2023-07-05 18:13:10 +0800

    神秘注释

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 function love.load()
     flandre = require 'flandre'
     _load_res 'script'
-    --_new(_class_player, _image.pl00, _quad_group.pl00_normal, _quad_group.pl00_left, _quad_group.pl00_right, _quad_group.pl00_leftex, _quad_group.pl00_rightex, 3)
 end
 
 function love.update()
